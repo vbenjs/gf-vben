a84eab711a772e4c79bc59e0f16841476cda8edd
JinMao <jinmao88@qq.com>
2021-04-09 15:57:55 +0800

    结构调整优化

--- app/service/response/response.go
+++ app/service/response/response.go
@@ -8,7 +8,7 @@ import (
 type JsonResponse struct {
 	Code    int         `json:"code"`    // 错误码((0:成功, 1:失败, >1:错误码))
 	Message string      `json:"message"` // 提示信息
-	Data    interface{} `json:"data"`    // 返回数据(业务接口定义具体数据结构)
+	Data    interface{} `json:"result"`  // 返回数据(业务接口定义具体数据结构)
 }
 
 // 标准返回结果数据结构封装。
