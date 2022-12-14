c7b92fdc04c282e47c96e8f5aab3bdf319761260
JinMao <jinmao88@qq.com>
2022-12-14 20:27:19 +0800

    curd controller优化

--- api/v1/curd.go
+++ api/v1/curd.go
@@ -1,9 +1,11 @@
 package v1
 
 import (
+	"Gf-Vben/internal/model"
 	"github.com/gogf/gf/v2/frame/g"
 )
 
 type CurdReq struct {
 	g.Meta `path:"/curd" method:"post" summary:"Curd请求" tags:"Curd"`
+	model.CurdReq
 }
--- internal/controller/curd.go
+++ internal/controller/curd.go
@@ -2,11 +2,9 @@ package controller
 
 import (
 	"Gf-Vben/api/v1"
-	"Gf-Vben/internal/model"
 	"Gf-Vben/internal/service"
 	"Gf-Vben/util"
 	"context"
-	"github.com/gogf/gf/v2/frame/g"
 )
 
 var (
@@ -17,10 +15,5 @@ type cCurd struct {
 }
 
 func (cCurd) Curd(ctx context.Context, req *v1.CurdReq) (res *util.JsonRes, err error) {
-	r := g.RequestFromCtx(ctx)
-	in := new(model.CurdReq)
-	if err := r.Parse(&in); err != nil {
-		return nil, err
-	}
-	return service.Curd().Curd(ctx, in)
+	return service.Curd().Curd(ctx, &req.CurdReq)
 }
