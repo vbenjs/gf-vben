39b3688d96ba147c93d155b145b07ffc69001302
JinMao <jinmao88@qq.com>
2024-09-18 14:29:13 +0800

    feat: 支持自行定义返回格式，通过中间件转换

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -10,26 +10,28 @@ import (
 func init() {
 }
 
+type Resp struct {
+	Code int         `json:"code" p:"code"`
+	Data interface{} `json:"result" p:"result"`
+	Msg  string      `json:"message" p:"message"`
+}
+
 func ResponseHandler(r *ghttp.Request) {
 	r.Middleware.Next()
-
 	// There's custom buffer content, it then exits current handler.
 	if r.Response.BufferLength() > 0 {
 		return
 	}
 	res := r.GetHandlerResponse()
 	err := r.GetError()
+	resp := new(Resp)
 	if err != nil {
-		r.Response.WriteJson(map[string]interface{}{
-			"code":    gerror.Code(err).Code(),
-			"message": err.Error(),
-		})
-
+		resp.Code = gerror.Code(err).Code()
+		resp.Msg = err.Error()
+		r.Response.WriteJson(resp)
 		return
 	}
-	m := gconv.Map(res)
-	m["data"] = m["result"]
-	delete(m, "result")
-	r.Response.WriteJson(m)
+	gconv.Scan(res, &resp)
+	r.Response.WriteJson(resp)
 
 }
